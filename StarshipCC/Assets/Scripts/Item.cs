26effaffa8d37b5060c45ef6ecd4c76bb6a8f4dd
gcmvy3 <grantmatthews28@yahoo.com>
2018-10-20 13:38:44 -0500

    Created Item class

---
+++ StarshipCC/Assets/Scripts/Item.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Item : MonoBehaviour {
+
+	// Use this for initialization
+	public virtual void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	public virtual void Update () {
+		
+	}
+
+    public abstract void OnEquip(PlayerController player);
+    public abstract void OnUnequip(PlayerController player);
+}
