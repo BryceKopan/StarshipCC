ead7ff3024d2da1c7dfc188b8a0e4f514fbdc3e1
BryceKopan <BryceKopan@gmail.com>
2018-11-12 03:49:23 -0600

    Added debris collision with projectils Added basic death effect to debris

---
+++ StarshipCC/Assets/Scripts/EnemyScripts/CapitalShip/Debris.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Debris : MonoBehaviour, Hittable
+{
+	public GameObject deathEffectPrefab;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void Death()
+	{
+		Destroy(gameObject);
+	}
+
+	void Hittable.OnHit(Projectile p)
+    {
+		Instantiate(
+                deathEffectPrefab,
+                transform.position,
+                transform.rotation);
+
+        Death();
+
+        p.Death();
+    }
+}
