8cf980d304cb25442f6c2327df73f358d59c93a2
gcmvy3 <grantmatthews28@yahoo.com>
2018-11-28 14:46:11 -0600

    Fixed bullet range bug

--- StarshipCC/Assets/Scripts/Bullet.cs
+++ StarshipCC/Assets/Scripts/Bullet.cs
@@ -3,4 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Bullet : Projectile {
+    public override void OnStart() 
+    {
+        base.OnStart();
+        damage = 1;
+        speed = 80;
+        range = 200;
+    }
 }
