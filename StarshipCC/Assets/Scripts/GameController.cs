9aeae451a50880ff39205a259f365f895f5d1028
BryceKopan <BryceKopan@gmail.com>
2018-11-29 15:42:49 -0600

    Added EndGame Screen -Added GameController to keep track of if players are alive -Added shityybackgroundsprite

---
+++ StarshipCC/Assets/Scripts/GameController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	public GameObject setActiveOnGameOver;
+
+	private List<GameObject> players;
+
+	// Use this for initialization
+	void Start () {
+		players = new List<GameObject>(GameObject.FindGameObjectsWithTag(Tags.PLAYER));
+
+		for(int i = 0; i < players.Count; i++)
+		{
+			if(players[i].name != "ship")
+			{
+				players.RemoveAt(i);
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(!isAPlayerAlive())
+		{
+			GameOver();
+		}
+	}
+
+	bool isAPlayerAlive()
+	{
+		int livingPlayerCount = players.Count;
+
+		foreach(GameObject player in players)
+		{
+			if(!player.activeSelf)
+			{
+				livingPlayerCount--;
+			}
+		}
+
+		if(livingPlayerCount <= 0)
+		{
+			return false;
+		}
+
+		return true;
+	}
+
+	void GameOver()
+	{
+		setActiveOnGameOver.SetActive(true);
+	}
+}
