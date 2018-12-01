404465121e7fedd74581240f31465cb595e4642e
BryceKopan <BryceKopan@gmail.com>
2018-11-30 18:25:59 -0600

    Added Coin SUCCC Added Coin Physics created folder for player scripts

--- StarshipCC/Assets/Scripts/Tags.cs
+++ StarshipCC/Assets/Scripts/Tags.cs
@@ -9,4 +9,5 @@ public class Tags
     public static string BULLET_SPAWN = "BulletSpawn";
     public static string PLAYER = "Player";
     public static string ENEMY = "Enemy";
+    public static string COIN = "Coin";
 }
