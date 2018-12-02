62122d19299512a5fd2fe19b519935ae24e63cfe
BryceKopan <BryceKopan@gmail.com>
2018-12-01 19:27:55 -0600

    Add Random Sprites Component Script

---
+++ StarshipCC/Assets/Scripts/Components/RandomSprite.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class RandomSprite : MonoBehaviour {
+
+	public List<Sprite> sprites;
+
+	void Start () 
+	{
+		int r = Random.Range(0, sprites.Count);
+		gameObject.GetComponent<SpriteRenderer>().sprite = sprites[r];
+	}
+}
