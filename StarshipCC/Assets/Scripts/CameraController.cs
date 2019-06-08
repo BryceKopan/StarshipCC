98826dc898940765865eb3c4dd1ead4d3efcb372
gcmvy3 <grantmatthews28@yahoo.com>
2019-06-08 02:28:22 -0500

    Fixed errors thrown by camera controller

--- StarshipCC/Assets/Scripts/CameraController.cs
+++ StarshipCC/Assets/Scripts/CameraController.cs
@@ -15,14 +15,30 @@ public class CameraController : MonoBehaviour {
 	{
         Vector2 avgPlayerPosition = Vector2.zero;
         PlayerController[] players = FindObjectsOfType<PlayerController>();
+
+        int numPositionsAveraged = 0;
+        CapitalController capitalShip = FindObjectOfType<CapitalController>();
+        Vector2 capitalShipPosition = Vector2.zero;
+        if(capitalShip)
+        {
+            capitalShipPosition = FindObjectOfType<CapitalController>().transform.position;
+        }
+
         foreach (PlayerController player in players)
         {
             avgPlayerPosition += new Vector2(player.transform.position.x, player.transform.position.y);
+            numPositionsAveraged++;
+
+            if(capitalShip)
+            {
+                avgPlayerPosition += new Vector2(player.transform.position.x, capitalShipPosition.y);
+                numPositionsAveraged++;
+            }   
         }
 
-        if(players.Length > 0)
+        if (avgPlayerPosition != Vector2.zero)
         {
-            avgPlayerPosition /= players.Length;
+            avgPlayerPosition /= numPositionsAveraged;
         }
 
         Vector2 moveVector = Vector2.MoveTowards(transform.position, avgPlayerPosition, moveSpeed * Time.deltaTime);
