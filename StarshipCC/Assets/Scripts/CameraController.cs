30f93abe682f1c2ccb9af29dec8789b5e5ab88d6
gcmvy3 <grantmatthews28@yahoo.com>
2019-06-08 02:38:35 -0500

    Fixed player jitter

--- StarshipCC/Assets/Scripts/CameraController.cs
+++ StarshipCC/Assets/Scripts/CameraController.cs
@@ -11,7 +11,7 @@ public class CameraController : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () 
+	void FixedUpdate () 
 	{
         Vector2 avgPlayerPosition = Vector2.zero;
         PlayerController[] players = FindObjectsOfType<PlayerController>();
