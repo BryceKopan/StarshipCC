9bcd3f707a07714a065ca31c976a8cf580b8e6c2
BryceKopan <BryceKopan@gmail.com>
2019-06-16 05:09:16 -0500

    Constructed Capital Ship -ConstructedCaptialController -New Captial Parts --Entrance --Corner --Fill -New Scene Constructed Ship 0 --Constructed Captial ship scene

---
+++ StarshipCC/Assets/Scripts/EnemyScripts/CapitalShip/ConstructedCapitalController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConstructedCapitalController : MonoBehaviour
+{
+    [SerializeField] List<GameObject> SmallAttachmentPrefabs;
+
+    void Start()
+    {
+        RandomizeShipParts();
+    }
+
+    void RandomizeShipParts()
+    {
+        List<GameObject> hardPoints = new List<GameObject>();
+
+        DynamicShipPart[] dynamicShipParts = transform.Find("Parts").GetComponentsInChildren<DynamicShipPart>();
+
+        foreach(DynamicShipPart part in dynamicShipParts)
+        {
+            GameObject newPart = part.Randomize();
+
+            Transform hardPoint = newPart.transform.Find("SmallHardPoint");
+            if(hardPoint != null)
+                hardPoints.Add(hardPoint.gameObject);
+        }
+
+        CreateAttachments(hardPoints);
+    }
+
+    void CreateAttachments(List<GameObject> hardPoints)
+    {
+        foreach(GameObject hardPoint in hardPoints)
+        {
+            int r = Random.Range(0, SmallAttachmentPrefabs.Count);
+
+            GameObject attachment = Instantiate(
+                SmallAttachmentPrefabs[r],
+                hardPoint.transform.position,
+                hardPoint.transform.rotation);
+
+            attachment.transform.SetParent(gameObject.transform.Find("Enemies")); 
+        }
+    }
+}
---
+++ StarshipCC/Assets/Scripts/EnemyScripts/CapitalShip/Parts/DynamicShipPart.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DynamicShipPart : MonoBehaviour
+{
+    [SerializeField] List<GameObject> possibleParts = new List<GameObject>();
+
+    public GameObject Randomize()
+    {
+        int r = Random.Range(0, possibleParts.Count);
+
+        GameObject newPart = Instantiate(possibleParts[r], transform.position, transform.rotation, transform.parent);
+        Destroy(gameObject);
+
+        return newPart;
+    }
+}
