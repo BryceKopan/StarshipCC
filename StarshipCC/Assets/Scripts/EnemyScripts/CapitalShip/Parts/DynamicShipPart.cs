0eb7bfcba8bc7e121a1b3b8cccef5917a24aaa5b
BryceKopan <BryceKopan@gmail.com>
2019-07-02 23:10:09 -0500

    Initial work on ShipSections complete -Base, Flat, Wing, ColumnHall

--- StarshipCC/Assets/Scripts/EnemyScripts/CapitalShip/Parts/DynamicShipPart.cs
+++ StarshipCC/Assets/Scripts/EnemyScripts/CapitalShip/Parts/DynamicShipPart.cs
@@ -5,6 +5,22 @@ using UnityEngine;
 public class DynamicShipPart : MonoBehaviour
 {
     [SerializeField] List<GameObject> possibleParts = new List<GameObject>();
+    List<GameObject> SmallAttachmentPrefabs = new List<GameObject>();
+
+    private Transform enemies;
+
+    void Start()
+    {
+        GameObject capitalShip = GameObject.Find("CapitalShip");
+        enemies = capitalShip.transform.Find("Enemies");
+        SmallAttachmentPrefabs = capitalShip.GetComponent<ConstructedCapitalController>().SmallAttachmentPrefabs;
+
+        GameObject newPart = Randomize();
+
+        Transform hardPoint = newPart.transform.Find("SmallHardPoint");
+        if(hardPoint != null)
+            CreateAttachment(hardPoint);
+    }
 
     public GameObject Randomize()
     {
@@ -14,5 +30,17 @@ public class DynamicShipPart : MonoBehaviour
         Destroy(gameObject);
 
         return newPart;
+    }
+
+    void CreateAttachment(Transform hardPoint)
+    {
+        int r = Random.Range(0, SmallAttachmentPrefabs.Count);
+
+        GameObject attachment = Instantiate(
+            SmallAttachmentPrefabs[r],
+            hardPoint.transform.position,
+            hardPoint.transform.rotation);
+
+        attachment.transform.SetParent(enemies); 
     }
 }
